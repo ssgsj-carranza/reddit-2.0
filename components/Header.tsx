bff4b6a64c746c4a1f3ddba97060f1933a0a466a
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-06-22 08:38:11 -0500

    whitelisted image link url, pulled it into header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <div className="relative h-10 w-20">
+            <Image src="https://links.papareact.com/fqy" layout="fill" />
+        </div>
+    </div>
   )
 }
 
