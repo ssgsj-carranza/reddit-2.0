5210238ead31b540b71d2c9aee6a72e1874c17c0
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-06-22 08:59:43 -0500

    added remaining icons and created custom utility styles for the icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
-import {ChevronDownIcon, HomeIcon, SearchIcon} from '@heroicons/react/solid';
-import {BeakerIcon} from '@heroicons/react/outline';
+import {ChevronDownIcon, HomeIcon, SearchIcon, MenuIcon} from '@heroicons/react/solid';
+import {BellIcon, ChatIcon, GlobeIcon, PlusIcon, SparklesIcon, SpeakerphoneIcon, VideoCameraIcon} from '@heroicons/react/outline';
 
 function Header() {
   return (
     <div className="flex bg-white px-4 py-2 shadow-sm sticky top-0 z-50">
         <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
-            <Image src="https://links.papareact.com/fqy" layout="fill" objectFit='contain'/>
+            <Image src="https://links.papareact.com/fqy" 
+                   layout="fill" 
+                   objectFit='contain'
+            />
         </div>
+        {/* right side */}
         <div className="flex items-center mx-7 xl:min-w-[300px]">
             <HomeIcon className="h-5 w-5" />
             <p className="flex-1 ml-2 hidden lg:inline">Home</p>
@@ -17,9 +21,23 @@ function Header() {
         {/* search */}
         <form className='flex flex-1 items-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1'>
             <SearchIcon className="h-6 w-6 text-gray-400"/>
-            <input className='flex-1 bg-transparent outline-none' type="text" placeholder='Search Reddit' />
+            <input className='flex-1 bg-transparent outline-none' 
+                   type="text" 
+                   placeholder='Search Reddit' 
+            />
             <button hidden type='submit' />
         </form>
+        {/* left side */}
+        <div className='flex'>
+            <SparklesIcon className="icon" />
+            <GlobeIcon className="icon" />
+            <VideoCameraIcon className="icon" />
+            <hr className='h-10 border border-gray-100' />
+            <ChatIcon className="icon" />
+            <BellIcon className="icon" />
+            <PlusIcon className="icon" />
+            <SpeakerphoneIcon className="icon" />
+        </div>
     </div>
   )
 }
