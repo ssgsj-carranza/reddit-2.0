ee170536955760f48fc46d77175ec2ed4f6b5210
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-06-23 09:31:47 -0500

    pulled in icons into post box, continuing styling

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,12 +1,13 @@
 import { useSession } from 'next-auth/react';
 import React from 'react'
 import Avatar from './Avatar';
+import {LinkIcon, PhotographIcon} from '@heroicons/react/outline';
 
 function PostBox() {
     const {data: session} = useSession();
 
   return (
-    <form>
+    <form className='sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2'>
         <div className="flex items-center space-x-3">
             {/* Avatar */}
             <Avatar />
@@ -15,6 +16,8 @@ function PostBox() {
                    disabled={!session}
                    className='bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1'
             />
+            <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+            <LinkIcon className='h-6 text-gray-300' />
         </div>
     </form>
   )
