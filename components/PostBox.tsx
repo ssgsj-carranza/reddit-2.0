4c871a97e50b3fc1505cf05724c4a8b719d10aec
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-07-29 12:54:28 -0500

    added styling and more fields to post box, created function to handle form submit

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,5 +1,5 @@
 import { useSession } from 'next-auth/react';
-import React from 'react'
+import React, { useState } from 'react'
 import Avatar from './Avatar';
 import {LinkIcon, PhotographIcon} from '@heroicons/react/outline';
 import {useForm} from 'react-hook-form'
@@ -14,9 +14,14 @@ type FormData = {
 function PostBox() {
     const {data: session} = useSession();
     const {register, setValue, handleSubmit, watch, formState: {errors}, } = useForm<FormData>();
+    const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false);
+
+    const onSubmit = handleSubmit(async(formData) => {
+        console.log(formData);
+    })
 
   return (
-    <form className='sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2'>
+    <form onSubmit={onSubmit} className='sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2'>
         <div className="flex items-center space-x-3">
             {/* Avatar */}
             <Avatar />
@@ -26,20 +31,57 @@ function PostBox() {
                    className='bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1'
                    {...register('postTitle', {required: true})}
             />
-            <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+            <PhotographIcon onClick={() => setImageBoxOpen(!imageBoxOpen)} 
+                            className={`h-6 text-gray-300 cursor-pointer ${imageBoxOpen && 'text-blue-300'}`} 
+            />
             <LinkIcon className='h-6 text-gray-300' />
         </div>
         {!!watch('postTitle') && (
-            <div>
+            <div className='flex flex-col py-2'>
                 {/* body */}
-                <div>
-                    <p>Body</p>
+                <div className='flex items-center px-2'>
+                    <p className='min-w-[90px]'>Body:</p>
                     <input {...register('postBody')} 
                            type='text' 
-                           placeholder='Text'
+                           placeholder='Text (optional)'
                            className='m-2 flex-1 bg-blue-50 p-2 outline-none' 
                     />
                 </div>
+                 {/* subreddit */}
+                 <div className='flex items-center px-2'>
+                    <p className='min-w-[90px]'>Subreddit:</p>
+                    <input {...register('subreddit', {required: true})} 
+                           type='text' 
+                           placeholder='Text (optional)'
+                           className='m-2 flex-1 bg-blue-50 p-2 outline-none' 
+                    />
+                </div>
+                {imageBoxOpen && (
+                    <div className='flex items-center px-2'>
+                    <p className='min-w-[90px]'>Image URL:</p>
+                    <input {...register('postImage')} 
+                           type='text' 
+                           placeholder='Optional...'
+                           className='m-2 flex-1 bg-blue-50 p-2 outline-none' 
+                    />
+                </div>
+                )}
+
+                {/* errors */}
+                {Object.keys(errors).length > 0 && (
+                    <div className='space-y-2 p-2 text-red-500'>
+                        {errors.postTitle?.type === 'required' && (
+                            <p>- A Post Title is required</p>
+                        )}
+                        
+                        {errors.subreddit?.type === 'required' && (
+                            <p>- A subreddit is required</p>
+                        )}
+                    </div>
+                )}
+                {!!watch('postTitle') && (
+                    <button type='submit' className='w-full rounded-full bg-blue-400 p-2 text-white'>Create Post</button>
+                )}
             </div>
         )}
     </form>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl my-7 mx-auto">
       <Head>
         <title>Reddit 2.0</title>
       </Head>
